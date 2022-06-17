0633a94c4c0d0bd00ea33ced6bb802b639d193cf
ogbaluChidiebubeDavid <102094211+ogbaluChidiebubeDavid@users.noreply.github.com>
2022-06-17 10:39:09 +0100

    Update models.py

--- I4G001078WCE/blog/models.py
+++ I4G001078WCE/blog/models.py
@@ -5,7 +5,7 @@ User = get_user_model()
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=23)
-    text = models.CharField
+    text = models.TextField
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField
     published_date = models.DateTimeField
